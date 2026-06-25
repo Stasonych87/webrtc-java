e2ec4fd32dd8189cd7e697d116891edafb6a06bf
Stasonych <petrashovStas@gmail.com>
2026-06-25 17:02:58 +0500

    try use self hosted mac agent

--- webrtc-jni/src/main/cpp/toolchain/x86_64-macos-cross.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-macos-cross.cmake
@@ -1,3 +1,4 @@
+message(STATUS "-----TEST CROSS 1")
 set(CMAKE_SYSTEM_NAME                  Darwin)
 set(CMAKE_SYSTEM_PROCESSOR             x86_64)
 
@@ -15,3 +16,4 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY  ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE  ONLY)
 
 set(TARGET_CPU                         "x64")
+message(STATUS "-----TEST CROSS 2")
