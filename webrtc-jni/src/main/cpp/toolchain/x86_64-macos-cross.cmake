4d965a50d4aa2840a4ef72979c1b779ce9042ade
Stasonych <petrashovStas@gmail.com>
2026-06-30 10:48:05 +0500

    try build for all OS

--- webrtc-jni/src/main/cpp/toolchain/x86_64-macos-cross.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-macos-cross.cmake
@@ -1,4 +1,3 @@
-message(STATUS "-----TEST CROSS 1")
 set(CMAKE_SYSTEM_NAME                  Darwin)
 set(CMAKE_SYSTEM_PROCESSOR             x86_64)
 
@@ -16,4 +15,3 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY  ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE  ONLY)
 
 set(TARGET_CPU                         "x64")
-message(STATUS "-----TEST CROSS 2")
